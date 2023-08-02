b723a9bec4a741cbb34980e7e7a395880cb88929
pocket-red-fox <pocket.red.fox@gmail.com>
2023-08-02 19:19:18 +0300

    fix: change link

--- lib/src/data/datasource/disney_datasource.dart
+++ lib/src/data/datasource/disney_datasource.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:disney_characters/src/data/models/character_mode.dart';
 
 import 'package:http/http.dart' as http;
@@ -11,16 +13,35 @@ class DisneyDatasourceImpl implements DisneyDatasource {
   @override
   Future<CharacterModel> getModelByID(String id) async {
     var url = Uri.https(
-      "disneyapi.dev",
+      "api.disneyapi.dev",
       "/character/$id",
     );
     final http.Response response = await http.get(url);
-    return CharacterModel.fromJson(response.body["data"]);
+    final jsonResult = jsonDecode(response.body)['data'];
+    return CharacterModel.fromMap(jsonResult);
   }
 
   @override
-  Future<List<CharacterModel>> getModelsByName(String name) {
-    // TODO: implement getModelsByName
-    throw UnimplementedError();
+  Future<List<CharacterModel>> getModelsByName(String name) async {
+    var url = Uri.https(
+      "api.disneyapi.dev",
+      "/character",
+      {'name': name},
+    );
+
+    final http.Response response = await http.get(url);
+    final jsonResult = jsonDecode(response.body)['data'];
+
+    if (jsonResult is Map<String, dynamic>) {
+      return [CharacterModel.fromMap(jsonResult)];
+    } else {
+      List<CharacterModel> models = [];
+
+      for (var element in (jsonResult as List)) {
+        models.add(CharacterModel.fromMap(element));
+      }
+
+      return models;
+    }
   }
 }
