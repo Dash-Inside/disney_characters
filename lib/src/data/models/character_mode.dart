71fae65aef2b6b37b0d14164aee0f09454686e53
Daniil A <92711103+LazySuperintelegense@users.noreply.github.com>
2023-08-02 15:47:52 +0300

    feat: create characters model

--- lib/src/data/models/character_mode.dart
+++ lib/src/data/models/character_mode.dart
@@ -1,3 +1,40 @@
 import 'package:disney_characters/src/domain/entities/character.dart';
 
-class CharacterModel extends Character {}
+import 'dart:convert';
+
+class CharacterModel extends Character {
+  const CharacterModel(
+      {required super.id,
+      required super.name,
+      required super.cinemas,
+      required super.imageLink});
+
+  factory CharacterModel.fromMap(Map<String, dynamic> map) {
+    List<String> films = (map["films"] as List<String>);
+    List<String> short = (map["shortFilms"] as List<String>);
+    List<String> tvShow = (map["tvShows"] as List<String>);
+    return CharacterModel(
+      id: (map['_id'] as int),
+      name: (map['Name'] as String),
+      cinemas: (films + short + tvShow),
+      imageLink: (map['imageUrl'] as String),
+    );
+  }
+
+  factory CharacterModel.fromJson(String source) {
+    return CharacterModel.fromMap(json.decode(source) as Map<String, dynamic>);
+  }
+
+  CharacterModel copyWith(
+    int? id,
+    String? name,
+    List<String>? cinemas,
+    String? imageLink,
+  ) {
+    return CharacterModel(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        cinemas: cinemas ?? this.cinemas,
+        imageLink: imageLink ?? this.imageLink);
+  }
+}
