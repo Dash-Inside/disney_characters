27426cd4e6f904eeae02c1b0ec31864bc11240d0
ShySpyDie <92712724+ShySpyDie@users.noreply.github.com>
2023-08-02 14:38:51 +0300

    feat: add colors

--- lib/src/presentation/app.dart
+++ lib/src/presentation/app.dart
@@ -5,6 +5,13 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: Color.fromRGBO(0, 159, 102, 1),
+          secondary: Color.fromRGBO(0, 0, 0, 1),
+        ),
+      ),
+    );
   }
 }
