9e2efcc315106ded4388096a6fd649021805a86f
Daniil A <92711103+LazySuperintelegense@users.noreply.github.com>
2023-08-02 17:45:16 +0300

    feat: create datasource GetModelById method

--- lib/src/data/datasource/disney_datasource.dart
+++ lib/src/data/datasource/disney_datasource.dart
@@ -1,8 +1,29 @@
 import 'package:disney_characters/src/data/models/character_mode.dart';
 
+import 'package:http/http.dart' as http;
+
 abstract class DisneyDatasource {
   Future<CharacterModel> getModelByID(String id);
   Future<List<CharacterModel>> getModelsByName(String name);
 }
 
-class DisneyDatasourceImpl implements DisneyDatasource {}
+class DisneyDatasourceImpl implements DisneyDatasource {
+  @override
+  Future<CharacterModel> getModelByID(String id) async {
+    var url = Uri.https(
+      "disneyapi.dev",
+      "/character",
+      {
+        "id": id,
+      },
+    );
+    final http.Response response = await http.get(url);
+    return CharacterModel.fromJson(response.body);
+  }
+
+  @override
+  Future<List<CharacterModel>> getModelsByName(String name) {
+    // TODO: implement getModelsByName
+    throw UnimplementedError();
+  }
+}
