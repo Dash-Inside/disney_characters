f1816590bd4570736153c38d7962c2d836d8e9dd
ShySpyDie <92712724+ShySpyDie@users.noreply.github.com>
2023-08-02 14:39:07 +0300

    feat: add appbar

--- lib/src/presentation/pages/home_page.dart
+++ lib/src/presentation/pages/home_page.dart
@@ -5,6 +5,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Input',
+          style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+        ),
+      ),
+    );
   }
 }
