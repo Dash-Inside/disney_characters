c04fbaa4e1c9808a8d4b2ea4539abbfa1381bf61
danwirele <115182119+danwirele@users.noreply.github.com>
2023-08-02 14:52:29 +0300

    fix: fix character entity

--- lib/src/domain/entities/character.dart
+++ lib/src/domain/entities/character.dart
@@ -1,10 +1,17 @@
-class Character {
+import 'package:equatable/equatable.dart';
+
+class Character extends Equatable {
+  final int id;
   final String name;
   final List<String> cinemas;
   final String imageLink;
-  Character({
+  const Character({
+    required this.id,
     required this.name,
     required this.cinemas,
     required this.imageLink,
   });
+
+  @override
+  List<Object?> get props => [id];
 }
