781fe8928b620b1bd28271e5122bbfe84693d26e
dexorded <126596523+dexorded@users.noreply.github.com>
2023-08-02 15:13:54 +0300

    feat: add character repository

--- lib/src/domain/repositories/character_repository.dart
+++ lib/src/domain/repositories/character_repository.dart
@@ -1,1 +1,12 @@
-abstract class CharacterRepository {}
+import 'package:dartz/dartz.dart';
+import 'package:disney_characters/core/failure/failure.dart';
+import 'package:disney_characters/src/domain/entities/character.dart';
+
+abstract class CharacterRepository {
+  /// [read] returns Either [Failure] if there is some errors or [Character] if there is no errors
+  /// [read] takes [id] that used for search unique [Character]
+  Future<Either<Failure, Character>> read({required int id});
+
+  /// [readAll] returns Either [Failure] if there is some errors or [List<Character>] if there is no errors
+  Future<Either<Failure, List<Character>>> readAll();
+}
