1768bdd92918587669beb3fc0dc3cc5c010a0dee
Daniil A <92711103+LazySuperintelegense@users.noreply.github.com>
2023-08-02 17:23:42 +0300

    feat: create get concrete char usecase

--- lib/src/domain/usecases/get_concrete_character_usecase.dart
+++ lib/src/domain/usecases/get_concrete_character_usecase.dart
@@ -5,15 +5,16 @@ import 'package:disney_characters/core/usecase/usecase.dart';
 import 'package:disney_characters/src/domain/entities/character.dart';
 import 'package:disney_characters/src/domain/repositories/character_repository.dart';
 
-class GetConceteCharactersUseCase extends UseCase<Character, String> {
+class GetConceteCharactersUseCase extends UseCase<Character, int> {
   final CharacterRepository characterRepository;
   GetConceteCharactersUseCase({
     required this.characterRepository,
   });
 
   @override
-  Future<Either<Failure, Character>> call(String p) {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<Either<Failure, Character>> call(int p) async {
+    Either<Failure, Character> concreteCharacter =
+        await characterRepository.read(id: p);
+    return concreteCharacter;
   }
 }
